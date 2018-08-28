0443dcdc31568de41552441b634a0923b7d859df
Aidan <dubsav@gmail.com>
2018-08-28 16:51:11 -0500

    added everything up to Goroutines

---
+++ Tutorial_Project/NewsAgg.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+)
+
+type SitemapIndex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword  string
+	Location string
+}
+
+type NewsAggPage struct {
+	Title string
+	News  map[string]NewsMap
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+	var s SitemapIndex
+	var n News
+	news_map := make(map[string]NewsMap)
+
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &s)
+
+	//index, value
+	for _, Location := range s.Locations {
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+
+		for index, _ := range n.Keywords {
+			news_map[n.Titles[index]] = NewsMap{n.Keywords[index], n.Locations[index]}
+		}
+	}
+	p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
+	t, _ := template.ParseFiles("newsaggtemplate.html")
+	t.Execute(w, p)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/agg/", newsAggHandler)
+	http.ListenAndServe(":8000", nil)
+}
