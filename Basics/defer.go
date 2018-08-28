d9a300752cacb5e39cbd543c47411615d479d25f
Aidan <dubsav@gmail.com>
2018-08-28 17:17:33 -0500

    added up to Panic and Recover

---
+++ Basics/defer.go
@@ -0,0 +1,19 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func foo() {
+// 	defer fmt.Println("Done!")
+// 	defer fmt.Println("Are we done?")
+// 	fmt.Println("Stuff")
+
+// 	for i := 0; i < 5; i++ {
+// 		defer fmt.Println(i)
+// 	}
+// }
+
+// func main() {
+// 	foo()
+// }
--- Basics/routines.go
+++ Basics/routines.go
@@ -9,12 +9,12 @@ import (
 var wg sync.WaitGroup
 
 func say(s string) {
+	defer wg.Done()
+
 	for i := 0; i < 3; i++ {
 		fmt.Println(s)
 		time.Sleep(time.Millisecond * 100)
-
 	}
-	wg.Done()
 }
 
 func main() {
