19d9443d9514852e2fb9659640fa9e59d3defd50
Aidan <dubsav@gmail.com>
2018-08-28 17:43:54 -0500

    added everything up to last tutorial

--- Basics/channels.go
+++ Basics/channels.go
@@ -1,20 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
 
 func foo(c chan int, someValue int) {
+	defer wg.Done()
 	c <- someValue * 5
 
 }
 
 func main() {
-	fooVal := make(chan int)
+	fooVal := make(chan int, 10)
 
-	go foo(fooVal, 5)
-	go foo(fooVal, 3)
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go foo(fooVal, i)
+	}
+	wg.Wait()
+	close(fooVal)
 
-	v1 := <-fooVal
-	v2 := <-fooVal
+	for item := range fooVal {
+		fmt.Println(item)
+	}
 
-	fmt.Println(v1, v2)
 }
